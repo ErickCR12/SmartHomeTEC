61de7eb24e48d945a423b5a1a9e8c4863950ad39
Erick Barrantes <erick.cr12@outlook.com>
2021-04-26 18:39:18 -0600

    GetDeviceTypes and AddDeviceType no longer user the DB context. Uses SQL scripts instead.

--- API_Service/Data/SmartHomeRepo.cs
+++ API_Service/Data/SmartHomeRepo.cs
@@ -1,17 +1,22 @@
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using API_Service.Models;
+using Npgsql;
 
 namespace API_Service.Data
 {
     public class SmartHomeRepo : ISmartHomeRepo
     {
 
+        private readonly NpgsqlConnection DBconn;
+
         private readonly SmartHomeDbContext _db;
         public SmartHomeRepo(SmartHomeDbContext db) 
         {
             _db = db;
+            DBconn = new NpgsqlConnection("Host=localhost;Port=5432;Username=smarthome;Password=emerson;Database=smarthome.dev;");
         }
 
         public IEnumerable<Device> GetAllDevices()
@@ -45,7 +50,29 @@ namespace API_Service.Data
 
         public IEnumerable<DeviceType> GetAllDeviceTypes()
         {
-            return _db.device_types.ToList();
+            //return _db.device_types.ToList();
+
+            List<DeviceType> allDeviceTypes = new List<DeviceType>();
+            
+            DBconn.Open();
+            var sqlCmd = new NpgsqlCommand(
+                "SELECT name, description, warranty_months " +
+                "FROM device_types", 
+                DBconn
+                );
+
+            NpgsqlDataReader DBreader = sqlCmd.ExecuteReader();
+            while(DBreader.Read())
+            {
+                DeviceType deviceType = new DeviceType();
+                deviceType.name = DBreader[0].ToString();
+                deviceType.description = DBreader[1].ToString();
+                deviceType.warranty_months = Int32.Parse(DBreader[2].ToString());
+                allDeviceTypes.Add(deviceType);
+            }
+            DBconn.Close();
+
+            return allDeviceTypes;
         }
 
         public DeviceType GetDeviceType(string name)
@@ -55,8 +82,18 @@ namespace API_Service.Data
 
         public void AddDeviceType(DeviceType deviceType)
         {
-            _db.Add(deviceType);
-            _db.SaveChanges();
+            // _db.Add(deviceType);
+            //_db.SaveChanges();
+
+            var sqlCmd = new NpgsqlCommand(
+                "INSERT INTO device_types (name, description, warranty_months) " +
+                "VALUES (@p1, @p2, @p3)", DBconn
+                );
+
+            sqlCmd.Parameters.AddWithValue("p1", deviceType.name);
+            sqlCmd.Parameters.AddWithValue("p2", deviceType.description);
+            sqlCmd.Parameters.AddWithValue("p3", deviceType.warranty_months);
+            sqlCmd.ExecuteNonQuery();
         }
 
         public void UpdateDeviceType(DeviceType deviceType)
