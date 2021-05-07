9c68a046408654d465f39a73831f06997500c1fd
Jarod <jarodavid@estudiantec.cr>
2021-05-06 20:12:39 -0600

    For items

--- WebApp/WebApp/src/app/app.component.ts
+++ WebApp/WebApp/src/app/app.component.ts
@@ -7,53 +7,97 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebApp';
+
+  //________/No Users\______
+  isAdmin: boolean = false;
   notLogged: boolean = false;
   notReg: boolean = false;
+
+  //________/Client\_______
   notEditProf: boolean = false;
   notShop: boolean = false;
   notReport: boolean = false;
+
+  //________/Admin \_______
   notDeviceType: boolean = false;
   notDevices: boolean = false;
   ActiveDevices: boolean = false;
   UsageDevices: boolean = false;
   UploadDoc: boolean = false;
 
+
+  //____________________/Client Tabs\_____________________
+  clickProfEdit() {
+    this.notEditProf = !this.notEditProf;
+    this.notShop = false;
+    this.notReport = false
+  }
+
+  clickShop() {
+    this.notShop = !this.notShop;
+    this.notEditProf = false;
+    this.notReport = false
+  }
+
+  clickReports() {
+    this.notReport = !this.notReport;
+    this.notEditProf = false;
+    this.notShop = false
+  }
+
+  //___________________/No User Tabs\______________________
   clickLog() {
     this.notLogged = !this.notLogged
   }
+
+  clickADMN() {
+    this.isAdmin = !this.isAdmin
+  }
+
   clickReg() {
     this.notReg = !this.notReg
   }
 
+  //____________________/Admin Tabs\______________________
   clickSeeDevs() {
     this.ActiveDevices = !this.ActiveDevices
+    this.UsageDevices = false;
+    this.notDevices = false;
+    this.notDeviceType = false;
+    this.UploadDoc = false
   }
 
   clickUsageDevices() {
-    this.UsageDevices = !this.UsageDevices
+    this.UsageDevices = !this.UsageDevices;
+    this.ActiveDevices = false;
+    this.notDevices = false;
+    this.notDeviceType = false;
+    this.UploadDoc = false
   }
 
   clickUploadDoc() {
     this.UploadDoc = !this.UploadDoc
+    this.UsageDevices = false;
+    this.ActiveDevices = false;
+    this.notDevices = false;
+    this.notDeviceType = false
   }
 
-  clickProfEdit() {
-    this.notEditProf = !this.notEditProf
-  }
-
-  clickShop() {
-    this.notShop = !this.notShop
-  }
-
-  clickReports() {
-    this.notReport = !this.notReport
-  }
   //type devices
   clickDeviceManager() {
-    this.notDeviceType = !this.notDeviceType
+    this.notDeviceType = !this.notDeviceType;
+    this.UsageDevices = false;
+    this.ActiveDevices = false;
+    this.notDevices = false;
+    this.UploadDoc = false
   }
 
   clickDevices() {
-    this.notDevices = !this.notDevices
+    this.notDevices = !this.notDevices;
+    this.UsageDevices = false;
+    this.ActiveDevices = false;
+    this.notDeviceType = false;
+    this.UploadDoc = false
   }
+
 }
